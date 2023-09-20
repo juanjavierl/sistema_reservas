8ded96b112ee526e818d9f0bd7f990444a8f4a86
JuanJavier <jjavierl.bo@gmail.com.com>
2023-09-20 11:56:00 -0400

    clase de Rutas URLs

--- app/catalogo/urls.py
+++ app/catalogo/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.catalogo, name='catalogo'),
+    path('produt/',views.productos,name='productos'),
+    path('producto/<int:id_producto>',views.detalleProducto,name='productoDetalle'),
 ] 
--- app/catalogo/views.py
+++ app/catalogo/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def catalogo(request):
     user = "Javier"#declaracion de una variable
     return render(request,'catalogo.html',{'user':user})
+
+
+
+def productos(request):
+    context = {}
+    return render(request, "productos.html",context)
+
+def detalleProducto(request, id_producto):
+    context = {
+        'id_producto':id_producto
+    }
+    return render(request, "productoDetalle.html",context)
